c244b72727c24a1dbf96d416ec822ba23db83038
Thompson Plyler <nealok.g@gmail.com>
2019-03-29 08:56:48 -0400

    Improvements for non-Chrome desktop browsing

--- crit.js
+++ crit.js
@@ -20,30 +20,37 @@ clickHandler = (e,crit) =>{
         fumble = false
     }
 
+    let frontText = crit.childNodes[1].childNodes[1].childNodes[0]//.childNodes[1]
     let backText = crit.childNodes[1].childNodes[3].childNodes[1]
-    let critType = (e.target.innerHTML).toLowerCase()
+    console.log("This is the text you see right away:", frontText)
+    let critType = e.target.innerText.toLowerCase()//.toLoweCase()
+    
+    console.log("CritType from event", critType)
 
 
     if (fumble && critType==="magic"){
         critType = "fmagic";
     }
-
-    cardTransition(e,crit, backText, critType)    
+    console.log(backText)
+    cardTransition(e,crit, backText, critType, frontText)    
     
 }
 
-cardTransition = (e,crit, backText, critType) => {
+cardTransition = (e,crit, backText, critType, frontText) => {
     if (crit.classList.contains("card-transform")){
         crit.classList.remove("card-transform")
+        frontText.classList.remove("card-front-vanish")
     }
     else {
-        critFetch(e,crit, backText, critType)
+        critFetch(e,crit, backText, critType, frontText)
     }
 }
 
 
-critFetch = (e,crit, backText, critType) => {
-    fetch(`https://fierce-bastion-18567.herokuapp.com/api/v1/crits?category=${critType}`,{
+critFetch = (e,crit, backText, critType, frontText) => {
+    console.log("e",e,"\n", "crit", crit,"\n", "backText", backText,"\n", "critType", critType,"\n", "frontText", frontText)
+    // fetch(`https://fierce-bastion-18567.herokuapp.com/api/v1/crits?category=${critType}`,{
+        fetch(`http://localhost:3001/api/v1/crits?category=${critType}`,{        
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -54,8 +61,8 @@ critFetch = (e,crit, backText, critType) => {
         })
         .then(critData => {
             console.log("Data from fetch", critData)
-            makeCard(critData,crit,backText, e)
-            showBack(e,crit)
+            makeCard(critData,crit,backText, e, frontText)
+            showBack(e,crit, frontText)
          } )
 
 }   
@@ -66,12 +73,15 @@ critFetch = (e,crit, backText, critType) => {
 
     cardDisplay= (critData, crit, backText) => {
         backText.innerHTML = ""
-        h1 = document.createElement("h1")
+        h1 = document.createElement("h2")
         h1.append(critData.name)
         backText.append(h1)
         backText.append(critData.description)
     }
 
-    showBack= (e,crit) =>{
+    showBack= (e,crit, frontText) =>{
+        console.log(frontText)
         crit.classList.add("card-transform")
+        frontText.classList.add("card-front-vanish")
+        
     }
